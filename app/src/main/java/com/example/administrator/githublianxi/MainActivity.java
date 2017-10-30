00981040bb8b4f8ce01caf48c2592feb13234b8a
ZhangMengQiao <33214101+ZhangMengQiao@users.noreply.github.com>
2017-10-30 19:59:22 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/administrator/githublianxi/MainActivity.java
+++ app/src/main/java/com/example/administrator/githublianxi/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //第一次更新
+        //第二次更新
     }
 }
